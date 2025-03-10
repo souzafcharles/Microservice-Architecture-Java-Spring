e2ddfe10b61e1ad5ffa3d1f4f40ad0375e1bb6c8
Charles Fernandes de Souza <souzach@outlook.com>
2025-03-10 16:36:01 -0300

    chore: delete src/main directory

--- src/main/java/com/souza/charles/accountservice/exceptions/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.souza.charles.accountservice.exceptions;
-
-public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/souza/charles/accountservice/exceptions/InvalidAccountException.java
+++
@@ -1,7 +0,0 @@
-package com.souza.charles.accountservice.exceptions;
-
-public class InvalidAccountException extends RuntimeException {
-    public InvalidAccountException(String message) {
-        super(message);
-    }
-}
